fb22e263c341117ef5b0541cf4d708633c74ae8c
liulinhui <liulinhui@dell>
2016-08-12 04:21:08 +0800

    shell

---
+++ src/Shell.java
@@ -0,0 +1,23 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Shell {
+
+	public static void main(String[] args) throws IOException {
+		try {
+			String commands = " tar -zxvf /home/liulinhui/文档/software/pycharm-professional-2016.2.tar.gz";
+			Process process = Runtime.getRuntime().exec(commands);
+			InputStreamReader ir = new InputStreamReader(process.getInputStream());
+			BufferedReader input = new BufferedReader(ir);
+			String line;
+			while ((line = input.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (java.io.IOException e) {
+			System.err.println("IOException " + e.getMessage());
+		}
+	}
+}
